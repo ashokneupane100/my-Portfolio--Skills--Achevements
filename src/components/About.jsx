fbc3af3ca7bb0c3c315b2bcf9fc2f72b41d843fc
nepalhomeland <ashokabrother@gmail.com>
2024-07-23 18:59:18 +0545

    About Component Done

---
+++ src/components/About.jsx
@@ -0,0 +1,73 @@
+export default function About() {
+  return (
+    <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16">
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!]
+        </span>
+
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills and Expertise:
+        </h1>
+        <span>
+          [Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!]
+        </span>
+
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills and Expertise:
+        </h1>
+        <span>
+          [Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!]
+        </span>
+
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements and awards:
+        </h1>
+        <span>
+          [Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!]
+        </span>
+
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statements:
+        </h1>
+        <p>
+          [Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reprehenderit aut pariatur! Repellendus incidunt error omnis
+          perferendis molestiae alias ullam doloribus! Aliquam, accusamus amet
+          aliquid natus sed veritatis aspernatur enim!]
+        </p>
+      </div>
+    </div>
+  );
+}
